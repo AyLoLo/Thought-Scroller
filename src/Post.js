885ca799812f3938a8366c6efef2b5091f76ce24
AnthonyLopez <anthonydlr66@gmail.com>
2023-08-30 20:34:13 -0400

    Updated CSS for component Post.js

--- src/Post.js
+++ src/Post.js
@@ -3,7 +3,17 @@ import React from "react";
 function Post({post}){
     return (
         <div className="text-white border-white md:border-4">
-            {post.title}
+            <h1 className="grid grid-rows-3">
+                <span className="border-b-4 border-white">{post.title}</span>
+                <span>By User</span>
+                <div className="grid grid-cols-2">
+                    <span>⬆️{post.vote_count}⬇️</span>
+                    <span>💬{post.comment_count}</span>
+                </div>
+            </h1>
+            <h1>
+
+            </h1>
         </div>
     )
 }
